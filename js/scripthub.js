86a9f119c1e08206fe2bfae7f57f7778ade44ffd
Leo Nesfield <leo@thelmgn.com>
2020-09-14 20:32:32 +0100

    debreak everything

--- js/scripthub.js
+++ js/scripthub.js
@@ -1,15 +1,3 @@
-
-
-function getScript(filename,cb) {
-    if (filename.startsWith("jellyapi:")) { // this is stored on a remote server, go fetch it
-        fetch(filename.replace("jellyapi:","")).then(function(ftch) {
-            ftch.text().then(function(t) {cb(null,t)}).catch((e) => {cb(e)})
-        }).catch((e) => {cb(e)})
-    } else { // it's on the local fs (i hope)
-        fs.readFile(filename,cb)
-    }
-}
-
 function selectScript(filename,strings) {
     return function() {
         var detailsHeader = document.querySelector("#scriptsDetails > h1")
@@ -21,7 +9,7 @@ function selectScript(filename,strings) {
             if (isLoading) return;
             isLoading = true
             
-            getScript(filename,(err,data) => {
+            jellyfish.getScript(filename,(err,data) => {
                 if (err) return console.error(err);
                 while (document.querySelector(".script.scriptSelected")) {document.querySelector(".script.scriptSelected").classList.remove("scriptSelected")}
                 this.classList.add("scriptSelected")
@@ -47,7 +35,7 @@ function createScript(p,filename) {
     var div = document.createElement("div")
     div.classList = "script"
     div.dataset.filename = filename
-    div.dataset.relative = path.join(p.dir,p.base)
+    div.dataset.relative = p.dir + "/" + p.base
     div.onclick = selectScript(filename,[p.base,p.dir])
     var title = document.createElement("h1")
     title.innerText = p.base
@@ -58,26 +46,7 @@ function createScript(p,filename) {
     scriptsContainer.appendChild(div)
 }
 
-var key = ""
 
-ipcRenderer.on('script-found', (event, arg) => {
-    if (arg[0] == key)  {
-        var r = path.relative(arg[1],arg[2])
-        var p = path.parse(r)
-        if (p.ext == ".lua" || p.ext == ".txt") {
-            createScript(p,arg[2])
-        }
-    } else {
-        ipcRenderer.cancel("cancelKey",key)
-    }
-})
-
-function startCrawl() {
-    isLoading = false
-    key = Math.random().toString()
-    scriptsContainer.innerHTML = ""
-    ipcRenderer.send("startCrawl",key)
-}
 searchBox.onchange = () => {
     var val = searchBox.value.toLocaleLowerCase()
     for (var s of document.querySelectorAll(".script"))  {
